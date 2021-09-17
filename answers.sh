438a210c2b088106fa63eb1ed71d397c143136c6
Noah Alexander Rogers <roger345@data.cs.purdue.edu>
2021-09-16 23:06:57 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,50 @@
 cd ORIGIN
 
 # Task 1 
-
+mv ~/homework-1-Z3yphr/ORIGIN/Homework/MA165/HW2.txt ~/homework-1-Z3yphr/ORIGIN/Homework/MA165/HW1.txt
 
 # Task 2
-
+rm -rf ~/homework-1-Z3yphr/ORIGIN/Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv ~/homework-1-Z3yphr/ORIGIN/Memes/'Star Wars'/fafsa.jpg ~/homework-1-Z3yphr/ORIGIN/Memes/SpongeBob
 
 # Task 4
-
+ rm -rf ~/homework-1-Z3yphr/ORIGIN/Homework/CS191
 
 # Task 5
-
+mkdir ./Programs/C++
 
 # Task 6
-
+mv ~/homework-1-Z3yphr/ORIGIN/Programs/Age.cpp ~/homework-1-Z3yphr/ORIGIN/Programs/C++/Age.cpp
 
 # Task 7
-
+mv ~/homework-1-Z3yphr/ORIGIN/Programs/Java ~/homework-1-Z3yphr/ORIGIN/Homework/CS180/Project_1
 
 # Task 8
+mv ~/homework-1-Z3yphr/ORIGIN/Programs/Count.c ~/homework-1-Z3yphr/ORIGIN/Programs/Goodbye.c ~/homework-1-Z3yphr/ORIGIN/Programs/C
 
 
 # Task 9
-
+mv ~/homework-1-Z3yphr/ORIGIN/Programs/Name.py ~/homework-1-Z3yphr/ORIGIN/Programs/Space.py ~/homework-1-Z3yphr/ORIGIN/Programs/'Python 3'
 
 # Task 10
-
+touch ~/homework-1-Z3yphr/ORIGIN/Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
 : '
 Type your vim/nano customizations below:
 
+set autoindent expandtab
+
+set mouse=a
+
+set number
+
+set showmatch
 
 
 '
